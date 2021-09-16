a1a73a09f9f801817004a7b27e00e1b112c6faec
Uros Dimitrijevic <urosrgf@gmail.com>
2021-09-16 12:36:21 +0200

    backup

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
+class Modal extends Component {
+
+    //improve performances
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+    
+    render() {
+        return (
+            <Aux>
         {/* backdrop - to nam je da pozadina bude zatamljena i da kliknemo na nju da nestane modal */}
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
         <div 
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}>
-            {props.children}
+            {this.props.children}
         </div>
     </Aux>
-)
+        )
+    }
+}
 
-export default modal;
+export default Modal;
